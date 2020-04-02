f153128fe4f45131d259a788d1acca57633f566a
Jesse <gumtow@gmail.com>
2020-04-01 22:47:15 -0500

    w08d2 after class commit Bingo game almost working

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-console.log('hey');
-console.log($);
+
+// OnLoad
 $(()=>{
 
 // Define Global Variables
@@ -9,62 +9,156 @@ const $cpu = $('#cpu');
 const $boardContainer = $('<div>').addClass('container');
 const $squareToMatch = $('.square-to-match');
 const $timer =$('#timer');
+let turn = "user";
+let myVar;
+const valArray = [];
+const valInPlay = [];
 
 
-$('body').append($boardContainer);
+
+// Generate Value Array
+const generateValues = () =>{
+    valArray.splice(0, valArray.length);
+    for (let i=1; i<=50; i++){
+        valArray.push(i);
+    };
+    console.log(valArray);
+}
+
 
 // Generate Random Number
-const randomNum = () =>{
-    let num = Math.floor((Math.random()*50)+1);
-    return num;
+const randomIndex = () =>{
+    let num = Math.floor((Math.random()*valArray.length));
+    let randomNum = valArray[num];
+    valArray.splice(num, 1);
+    return randomNum;
 }
 
 // Generate the Board
 const generateBoard = (player) =>{
+    $('body').append($boardContainer);
         $boardContainer.empty();
+        generateValues();
         for (let i=1; i<=25; i++){
-            const $square = $('<div>').addClass('child').addClass(`child-${i}`).text(randomNum());
+            
+            // generate 25 squares with randoms numbers
+            const $square = $('<div>').addClass('child').attr('id', i).addClass('open').text(randomIndex());
+            valInPlay.push($square.text());
+            
+            // Mouse interactions
             $square.on('click', (event)=>{
-                $(event.target).css({'background-color':'rgb(212, 104, 64)', 'color':'#ffffff'});
-                console.log('clicked!')
+                if (turn === "user") {
+                    if ($square.text() === $squareToMatch.text()){
+                        $(event.target).removeClass('open').addClass('matched').css({'background-color':'rgb(212, 104, 64)', 'color':'#ffffff'});
+                        turn = "cpu";
+                        const removeIndex = valInPlay.indexOf($square.text());
+                        valInPlay.splice(removeIndex, 1);
+                        checkWinner('#user');
+                        cpuMatch();
+                        squareMatch();
+                    };
+                };
             });
-            $square.mouseenter((evt)=>{
-                $(event.target).css({'background-color':'coral', 'color':'#ffffff'});
+            $square.mouseenter((event)=>{
+                if ($(event.target).hasClass('open')){
+                    if (turn === "user"){
+                        $(event.target).css({'background-color':'coral', 'color':'#ffffff'});
+                    };
+                };
             });
-            $square.mouseleave((evt)=>{
-                $(event.target).css({'background-color':'cornsilk', 'color':'#000000'});
+            $square.mouseleave((event)=>{
+                if ($(event.target).hasClass('open')){
+                    $(event.target).css({'background-color':'cornsilk', 'color':'#000000'});
+                };
             });
+            
+            // Append square to the board
             player.append($square);
+
         }
     }
+
+// check to see if CPU has match
+const cpuMatch = () =>{
+    for (let i=1; i<=25; i++){
+            const $cpuId = $(`#cpu > #${i}`);
+            if ($cpuId.text() === $squareToMatch.text()){
+                $cpuId.removeClass('open').addClass('matched').css({'background-color':'rgb(212, 104, 64)', 'color':'#ffffff'});
+                        turn = "user";
+                        const removeIndex = valInPlay.indexOf($cpuId.text());
+                        console.log(removeIndex);
+                        valInPlay.splice(removeIndex, 1);
+                        // console.log(valInPlay);
+                        
+            }
+    }
+    turn = "user";
+    checkWinner('#cpu');
+    squareMatch();
+}
+
+
 
 // Generate Square to Match
 const squareMatch=()=>{
     $squareToMatch.empty();
-    $squareToMatch.text(randomNum());
-    
-    timer(10);
-    
+    $squareToMatch.text(valInPlay[Math.floor(Math.random()*valInPlay.length)]); 
+    // cpuMatch();
+    // timer(10);    
 }
 
 // Set and start Timer
-const timer = (time)=>{
-    let timeLeft = time;
-    $timer.text(timeLeft);
-    myVar = setInterval(()=>{
-        timeLeft--;
-        if (timeLeft >= 0){
-            $timer.text(timeLeft);
-        } else if (timeLeft < 0){
-            $timer.text(timeLeft);
-            clearInterval(myVar);
-            squareMatch();
-            // console.log(myVar);
-              
-        }
-    }, 1000);
+// const timer = (time)=>{
+//     let timeLeft = time;
+//     $timer.text(timeLeft);
+//     myVar = setInterval(()=>{
+//         timeLeft--;
+//         if (timeLeft >= 0){
+//             $timer.text(timeLeft);
+//         } else if (timeLeft < 0){
+//             $timer.text(timeLeft);
+//             clearInterval(myVar);
+//             squareMatch();          
+//         }
+//     }, 1000);
     
-};
+// };
+
+// Check for a winner
+
+const checkWinner =(player)=>{
+    if ($(`${player} > #1`).hasClass('matched') && $(`${player} > #2`).hasClass('matched') && $(`${player} > #3`).hasClass('matched') && $(`${player} > #4`).hasClass('matched') && $(`${player} > #5`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #6`).hasClass('matched') && $(`${player} > #7`).hasClass('matched') && $(`${player} > #8`).hasClass('matched') && $(`${player} > #9`).hasClass('matched') && $(`${player} > #10`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #11`).hasClass('matched') && $(`${player} > #12`).hasClass('matched') && $(`${player} > #13`).hasClass('matched') && $(`${player} > #14`).hasClass('matched') && $(`${player} > #15`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #16`).hasClass('matched') && $(`${player} > #17`).hasClass('matched') && $(`${player} > #18`).hasClass('matched') && $(`${player} > #19`).hasClass('matched') && $(`${player} > #20`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #21`).hasClass('matched') && $(`${player} > #22`).hasClass('matched') && $(`${player} > #23`).hasClass('matched') && $(`${player} > #24`).hasClass('matched') && $(`${player} > #25`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #1`).hasClass('matched') && $(`${player} > #6`).hasClass('matched') && $(`${player} > #11`).hasClass('matched') && $(`${player} > #16`).hasClass('matched') && $(`${player} > #21`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #2`).hasClass('matched') && $(`${player} > #7`).hasClass('matched') && $(`${player} > #12`).hasClass('matched') && $(`${player} > #17`).hasClass('matched') && $(`${player} > #22`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #3`).hasClass('matched') && $(`${player} > #8`).hasClass('matched') && $(`${player} > #13`).hasClass('matched') && $(`${player} > #18`).hasClass('matched') && $(`${player} > #23`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #4`).hasClass('matched') && $(`${player} > #9`).hasClass('matched') && $(`${player} > #14`).hasClass('matched') && $(`${player} > #19`).hasClass('matched') && $(`${player} > #24`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #5`).hasClass('matched') && $(`${player} > #10`).hasClass('matched') && $(`${player} > #15`).hasClass('matched') && $(`${player} > #20`).hasClass('matched') && $(`${player} > #25`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #1`).hasClass('matched') && $(`${player} > #7`).hasClass('matched') && $(`${player} > #13`).hasClass('matched') && $(`${player} > #19`).hasClass('matched') && $(`${player} > #25`).hasClass('matched')) {
+        endGame(player);
+    } else if ($(`${player} > #5`).hasClass('matched') && $(`${player} > #9`).hasClass('matched') && $(`${player} > #13`).hasClass('matched') && $(`${player} > #17`).hasClass('matched') && $(`${player} > #21`).hasClass('matched')) {
+        endGame(player);
+        
+    };
+}
+
+const endGame=(player)=>{
+    alert(`${player} won!!!`)
+    clearInterval(myVar);
+}
 
 
 
@@ -72,6 +166,8 @@ const timer = (time)=>{
 
     generateBoard($user);
     generateBoard($cpu);
+    // console.log(valInPlay);
     squareMatch();
-    // timer(10);
+    // const whichPlayer = '#user';
+    // console.log($(`${whichPlayer} > #1`));
 });
