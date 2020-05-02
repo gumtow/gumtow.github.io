5e47164a225503f4ce6b0528bb9af2e8ae409b3a
Jesse <gumtow@gmail.com>
2020-05-02 13:30:30 -0500

    file structure and dependencies installed.

--- project2-full-stack-app/expServerStart.sh
+++ project2-full-stack-app/expServerStart.sh
@@ -1,9 +1,9 @@
 #!/bin/sh  
 touch server.js
 mkdir views public models controllers
-mkdir models/css
-mkdir models/img
-touch models/css/style.css
+mkdir public/css
+mkdir public/img
+touch public/css/style.css
 touch views/Index.jsx
 touch views/Show.jsx
 touch views/New.jsx 
@@ -13,6 +13,5 @@ touch models/portfolio.js
 touch controllers/portfolio.js
 npm init -y
 npm i express express-react-views react-dom react method-override mongoose
-code package.json server.js Index.jsx Show.jsx New.jsx
 git add .
 git commit -m "file structure and dependencies installed."
